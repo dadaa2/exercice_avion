1a3f879d645ec449608715c9827d225c0c8572b5
David <dhuang@uuds.com>
2024-11-22 14:40:00 +0100

    Bootstrap mini fix sur le formulaire

--- src/Form/InterventionAjoutType.php
+++ src/Form/InterventionAjoutType.php
@@ -29,23 +29,28 @@ class InterventionAjoutType extends AbstractType
             ->add('InterventionDateDebut', null, [
                 'widget' => 'single_text',
                 'required'=> true,
-                'label' => "Date du début de l'intervention"
+                'label' => "Date du début de l'intervention",
+                'attr' => ['class' => 'form-control'],
             ])
             ->add('InterventionDateFin', null, [
                 'widget' => 'single_text',
                 'required'=> true,
-                'label' => "Date de fin de l'intervention"
+                'label' => "Date de fin de l'intervention",
+                'attr' => ['class' => 'form-control'],
             ])
             
             ->add('InterventionType', EntityType::class, ['class' => TypeIntervention::class, 'choice_label' => 'typeInterventionNom',
-                'label' => "Type d'intervention à affecter "
+                'label' => "Type d'intervention à affecter ",
+                'attr' => ['class' => 'form-control'],
             ])
 
             ->add('InterventionCommentaire', options:[
-                'label' => "Type d'intervention à affecter"
+                'label' => "Commentaire de l'intervention ",
+                'attr' => ['class' => 'form-control'],
                 ])
             ->add('submit', SubmitType::class, options: [
                 'label' => "Ajouter l'intervention",
+                'attr' => ['class' => 'btn btn-primary'],
                 ])
 
         ;
