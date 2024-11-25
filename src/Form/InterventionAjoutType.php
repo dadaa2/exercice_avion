13cfef80f47478f9fcba5720be29297508ec0227
David <dhuang@uuds.com>
2024-11-25 13:16:18 +0100

    fix traduction et bootstrap

--- src/Form/InterventionAjoutType.php
+++ src/Form/InterventionAjoutType.php
@@ -10,9 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Translation\TranslatableMessage;
 
 class InterventionAjoutType extends AbstractType
 {
