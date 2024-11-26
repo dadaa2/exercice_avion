ba5489e1c0828a05dc8e2e7d671f2582831649f2
David <dhuang@uuds.com>
2024-11-26 16:06:08 +0100

    Creation des crud de statues via commande

--- src/Controller/StatuesController.php
+++ src/Controller/StatuesController.php
@@ -2,40 +2,80 @@
 
 namespace App\Controller;
 
-use App\Repository\StatuesRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Statues;
 use App\Form\StatuesType;
+use App\Repository\StatuesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-
-#[Route('/statues', name: 'app_statues_')]
-class StatuesController extends AbstractController
+#[Route('/statues')]
+final class StatuesController extends AbstractController
 {
-    #[Route('/', name: 'liste_statues')]
-    public function index(
-        StatuesRepository $statuesRepository,
-    ): Response
+    #[Route(name: 'app_statues_index', methods: ['GET'])]
+    public function index(StatuesRepository $statuesRepository): Response
     {
-        $statues = $statuesRepository->findAll();
-        //dd(vars: $statues);
-
         return $this->render('statues/index.html.twig', [
-            'statues' => $statues,
+            'statues' => $statuesRepository->findAll(),
         ]);
     }
 
-    #[Route('/add', name:'add', methods: ['GET', 'POST'])]
-    public function add(
-        Statues $statues,
-        ): Response
-        {
-            $statue = new Statues();
-            $statueForm = $this->createForm(StatuesType::class, $statues);
+    #[Route('/new', name: 'app_statues_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $statue = new Statues();
+        $form = $this->createForm(StatuesType::class, $statue);
+        $form->handleRequest($request);
 
-            return $this->render('statues/add.html.twig', [
-                'statueForm' => $statueForm->createView(),
-            ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($statue);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_statues_index', [], Response::HTTP_SEE_OTHER);
         }
+
+        return $this->render('statues/new.html.twig', [
+            'statue' => $statue,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_statues_show', methods: ['GET'])]
+    public function show(Statues $statue): Response
+    {
+        return $this->render('statues/show.html.twig', [
+            'statue' => $statue,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_statues_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Statues $statue, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(StatuesType::class, $statue);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_statues_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('statues/edit.html.twig', [
+            'statue' => $statue,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_statues_delete', methods: ['POST'])]
+    public function delete(Request $request, Statues $statue, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$statue->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($statue);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_statues_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
